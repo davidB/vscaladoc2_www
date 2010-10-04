3d1e2984d1c556fec7c08d3c46a4bb9a406de5dd
David Bernard <david.bernard.31@gmail.com>
2010-10-04 15:21:34 +0200

    refactor Boot

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,5 +1,6 @@
 package bootstrap.liftweb
 
+import net.liftweb.mapper.MapperRules
 import net_alchim31_vscaladoc2_www.view.ApiView
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
@@ -16,60 +17,21 @@ import _root_.net_alchim31_vscaladoc2_www.model._
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
-class Boot {
+class Boot extends Loggable {
   def boot {
-    initDB()
-    
-    // lets add Scalate
-    //    val scalateView = new ScalateView
-    //    scalateView.register
-    LiftRules.statelessDispatchTable.append(ApiView.dispatch)
-
     // where to search snippet
     LiftRules.addToPackages("net_alchim31_vscaladoc2_www")
 
-
-    // Build SiteMap
-    def sitemap() = List( 
-    		Menu("Home") / "index" >> LocGroup("public"),
-    		Menu("Admin") / "admin" / "index" >> LocGroup("admin"),
-    		Menu("Apis") / "admin" / "apis" >> LocGroup("admin")
-    		  submenus(RemoteApiInfo.menus : _*)
-//      // Menu with special Link
-//      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
-//        "Static Content")) ::
-      // Menu entries for the User management stuff
-      ) ::: User.menus
-
-    LiftRules.setSiteMap(SiteMap(sitemap : _*))
-
-    /*
-     * Show the spinny image when an Ajax call starts
-     */
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-
-    /*
-     * Make the spinny image go away when it ends
-     */
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    LiftRules.early.append(makeUtf8)
-    LiftRules.useXhtmlMimeType = false // recaptcha js lib
+    configureRDBMS()
     
-    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+    configureHttpRequest()
 
+    configureUserExperience()
+    
+    logger.debug("DEBUG MODE ENABLED!")
   }
 
-  /**
-   * Force the request to be UTF-8
-   */
-  private def makeUtf8(req: HTTPRequest) {
-    req.setCharacterEncoding("UTF-8")
-  }
-
-  private def initDB() {
+  private def configureRDBMS() {
     if (!DB.jndiJdbcConnAvailable_?) {
       val vendor =
         new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
@@ -81,10 +43,59 @@ class Boot {
 
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
-    Schemifier.schemify(true, Schemifier.infoF _, User)
-    Schemifier.schemify(true, Schemifier.infoF _, RemoteApiInfo)
+    
+    MapperRules.columnName = (_,name) => StringHelpers.snakify(name)
+    MapperRules.tableName =  (_,name) => StringHelpers.snakify(name)
+    
+    Schemifier.schemify(true, Schemifier.infoF _, User, RemoteApiInfo)
     
     RemoteApiInfo.init()
+    
     S.addAround(DB.buildLoanWrapper)
+  }
+  
+  private def configureHttpRequest() {
+    // Build SiteMap
+    val MustBeLoggedIn = If(() => User.loggedIn_?, "")
+    def sitemap() = List( 
+    		Menu("Home") / "index" >> LocGroup("public"),
+    		Menu("Admin") / "admin" / "index" >> LocGroup("admin") >> MustBeLoggedIn,
+    		Menu("Apis") / "admin" / "apis" >> LocGroup("admin") >> MustBeLoggedIn
+    		  submenus(RemoteApiInfo.menus : _*)
+//      // Menu with special Link
+//      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
+//        "Static Content")) ::
+      // Menu entries for the User management stuff
+      ) ::: User.menus
+
+    LiftRules.setSiteMap(SiteMap(sitemap : _*))
+
+    // setup the 404 handler 
+//    LiftRules.uriNotFound.prepend(NamedPF("404handler"){
+//      case (req,failure) => NotFoundAsTemplate(ParsePath(List("404"),"html",false,false))
+//    })
+
+    // lets add Scalate
+    //    val scalateView = new ScalateView
+    //    scalateView.register
+    LiftRules.statelessDispatchTable.append(ApiView.dispatch)
+
+    // make requests utf-8, html
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))	  
+    LiftRules.useXhtmlMimeType = false // recaptcha js lib
+  }
+  
+  private def configureUserExperience() {
+
+    // set the time that notices should be displayed and then fadeout
+    LiftRules.noticesAutoFadeOut.default.set((notices: NoticeType.Value) => Full(2 seconds, 2 seconds))
+
+    /*
+     * Show the spinny image when an Ajax call starts
+     */
+    LiftRules.ajaxStart = Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    LiftRules.ajaxEnd = Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+    
+    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
   }
 }
